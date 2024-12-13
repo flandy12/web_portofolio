385fedc8a76b5327064e9efa9a28d04e3f2c52b0
Mona Lisa <halloflandy@gmail.com>
2024-12-13 14:05:46 +0700

    add chat.js

--- src/pages/components/SalesChart.js
+++ src/pages/components/SalesChart.js
@@ -256,120 +256,120 @@ export default function CardLineChart() {
 
       <div>
 
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Product name
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Color
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Category
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Stock
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Silver
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Laptop
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             65 %
                         </td>
                     </tr>
-                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Microsoft Surface Pro
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             White
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Laptop PC
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             78 %
                         </td>
                     </tr>
-                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Magic Mouse 2
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Black
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Accessories
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                          66 %
                         </td>
                     </tr>
-                    <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Google Pixel Phone
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Gray
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Phone
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             44 %
                         </td>
                     </tr>
                     <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Apple Watch 5
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Red
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Wearables
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             56 %
                         </td>
                     </tr>
                     <tr  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Apple Watch 5
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Red
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Wearables
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             67 %
                         </td>
                     </tr>
                     <tr  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Apple Watch 5
                         </th>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Red
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             Wearables
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             75 %
                         </td>
                     </tr>
