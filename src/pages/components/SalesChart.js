675db05b4ca68d8cb7343f45941e5bbc8aaa24c3
Mona Lisa <halloflandy@gmail.com>
2024-12-20 10:24:15 +0700

    update chart

--- src/pages/components/SalesChart.js
+++ src/pages/components/SalesChart.js
@@ -2,109 +2,13 @@ import React from "react";
 import Chart from "chart.js";
 
 export default function CardLineChart() {
+
+  const dataDiagram = {
+    '2023': [2416, 4136, 7935, 8004, 9505, 5026, 6108, 6343, 9404, 9280, 9287, 8689],
+    '2024': [4017, 6135, 7091, 5841, 5036, 4547, 3467, 3970, 6313, 3595, 9207, 594]
+  }
+
   React.useEffect(() => {
-    var config = {
-      type: "line",
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-        ],
-        datasets: [
-          {
-            label: new Date().getFullYear(),
-            backgroundColor: "#3182ce",
-            borderColor: "#3182ce",
-            data: [65, 78, 66, 44, 56, 67, 75],
-            fill: false,
-          },
-          {
-            label: new Date().getFullYear() - 1,
-            fill: false,
-            backgroundColor: "#edf2f7",
-            borderColor: "#edf2f7",
-            data: [40, 68, 86, 74, 56, 60, 87],
-          },
-        ],
-      },
-      options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        title: {
-          display: false,
-          text: "Sales Charts",
-          fontColor: "white",
-        },
-        legend: {
-          labels: {
-            fontColor: "white",
-          },
-          align: "end",
-          position: "bottom",
-        },
-        tooltips: {
-          mode: "index",
-          intersect: false,
-        },
-        hover: {
-          mode: "nearest",
-          intersect: true,
-        },
-        scales: {
-          xAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Month",
-                fontColor: "white",
-              },
-              gridLines: {
-                display: false,
-                borderDash: [2],
-                borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.3)",
-                zeroLineColor: "rgba(0, 0, 0, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Value",
-                fontColor: "white",
-              },
-              gridLines: {
-                borderDash: [3],
-                borderDashOffset: [3],
-                drawBorder: false,
-                color: "rgba(255, 255, 255, 0.15)",
-                zeroLineColor: "rgba(33, 37, 41, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-        },
-      },
-    };
-
-
     var config_bar = {
         type: "bar",
         data: {
@@ -122,7 +26,7 @@ export default function CardLineChart() {
               label: new Date().getFullYear(),
               backgroundColor: "#3182ce",
               borderColor: "#3182ce",
-              data: [65, 78, 66, 44, 56, 67, 75],
+              data: [4017, 6135, 7091, 5841, 5036, 4547, 3467, 3970,6313, 3595, 9207, 5945],
               fill: false,
             },
             {
@@ -130,7 +34,7 @@ export default function CardLineChart() {
               fill: false,
               backgroundColor: "#edf2f7",
               borderColor: "#edf2f7",
-              data: [40, 68, 86, 74, 56, 60, 87],
+              data: [2416, 4136, 7935, 8004, 9505, 5026, 6108, 6343,9404, 9280, 9287, 8689],
             },
           ],
         },
@@ -206,15 +110,13 @@ export default function CardLineChart() {
         },
       };
 
-    var ctx = document.getElementById("line-chart").getContext("2d");
-    window.myLine = new Chart(ctx, config);
-
     var ctx = document.getElementById("line-chart2").getContext("2d");
     window.myLine = new Chart(ctx, config_bar);
   }, []);
   return (
     <>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1">
+       
         <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
             <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
             <div className="flex flex-wrap items-center">
@@ -222,26 +124,7 @@ export default function CardLineChart() {
                 <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
                     Overview
                 </h6>
-                <h2 className="text-white text-xl font-semibold">Sales value</h2>
-                </div>
-            </div>
-            </div>
-            <div className="p-4 flex-auto">
-            {/* Chart */}
-            <div className="relative h-350-px">
-                <canvas id="line-chart"></canvas>
-            </div>
-            </div>
-        </div>
-
-        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
-            <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
-            <div className="flex flex-wrap items-center">
-                <div className="relative w-full max-w-full flex-grow flex-1">
-                <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
-                    Overview
-                </h6>
-                <h2 className="text-white text-xl font-semibold">Sales value</h2>
+                <h2 className="text-white text-xl font-semibold">Dashboard Penjualan Tahunan</h2>
                 </div>
             </div>
             </div>
@@ -256,125 +139,44 @@ export default function CardLineChart() {
 
       <div>
 
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Product name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Color
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Stock
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td className="px-6 py-4">
-                            65 %
-                        </td>
-                    </tr>
-                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-6 py-4">
-                            White
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop PC
-                        </td>
-                        <td className="px-6 py-4">
-                            78 %
-                        </td>
-                    </tr>
-                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-6 py-4">
-                            Black
-                        </td>
-                        <td className="px-6 py-4">
-                            Accessories
-                        </td>
-                        <td className="px-6 py-4">
-                         66 %
-                        </td>
-                    </tr>
-                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Google Pixel Phone
-                        </th>
-                        <td className="px-6 py-4">
-                            Gray
-                        </td>
-                        <td className="px-6 py-4">
-                            Phone
-                        </td>
-                        <td className="px-6 py-4">
-                            44 %
-                        </td>
-                    </tr>
-                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple Watch 5
-                        </th>
-                        <td className="px-6 py-4">
-                            Red
-                        </td>
-                        <td className="px-6 py-4">
-                            Wearables
-                        </td>
-                        <td className="px-6 py-4">
-                            56 %
-                        </td>
-                    </tr>
-                    <tr  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple Watch 5
-                        </th>
-                        <td className="px-6 py-4">
-                            Red
-                        </td>
-                        <td className="px-6 py-4">
-                            Wearables
-                        </td>
-                        <td className="px-6 py-4">
-                            67 %
-                        </td>
-                    </tr>
-                    <tr  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple Watch 5
-                        </th>
-                        <td className="px-6 py-4">
-                            Red
-                        </td>
-                        <td className="px-6 py-4">
-                            Wearables
-                        </td>
-                        <td className="px-6 py-4">
-                            75 %
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
+          <table id="data-table" className="w-full">
+              <thead>
+                  <tr>
+                      <th rowspan="2" className="border border-gray-700 px-4 py-2">Tahun</th>
+                      <th colspan="12" className="border border-gray-700 px-4 py-2">Bulan</th>
+                  </tr>
+                  <tr>
+                    <th className="border border-gray-700 px-4 py-2">Januari</th>
+                    <th className="border border-gray-700 px-4 py-2">Februari</th>
+                    <th className="border border-gray-700 px-4 py-2">Maret</th>
+                    <th className="border border-gray-700 px-4 py-2">April</th>
+                    <th className="border border-gray-700 px-4 py-2">Mei</th>
+                    <th className="border border-gray-700 px-4 py-2">Juni</th>
+                    <th className="border border-gray-700 px-4 py-2">Juli</th>
+                    <th className="border border-gray-700 px-4 py-2">Agustus</th>
+                    <th className="border border-gray-700 px-4 py-2">September</th>
+                    <th className="border border-gray-700 px-4 py-2">Oktober</th>
+                    <th className="border border-gray-700 px-4 py-2">November</th>
+                    <th className="border border-gray-700 px-4 py-2">Desember</th>
+                  </tr>
+              </thead>            
+              <tbody>
+                {Object.entries(dataDiagram).map(([year, data]) => (
+                  <tr key={year} className="gap-5">
+                    <td rowSpan={1} className="border border-gray-700 px-4 py-2 text-center">
+                      {year}
+                    </td>
+                    {data.map((value, index) => (
+                      <td key={`${year}-${index}`} className="border border-gray-700 px-4 py-2 text-center">
+                        {value}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot id="totalRow"></tfoot>
+          </table>
         </div>
 
       </div>
